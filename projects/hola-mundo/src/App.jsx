09435d4af023430b5b7abeb25b971caa3973e505
Luis <veachairezluisangel@gmail.com>
2025-01-15 15:44:47 -0700

    Terminada primera clase

--- projects/hola-mundo/src/App.jsx
+++ projects/hola-mundo/src/App.jsx
@@ -1,23 +1,45 @@
 import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
+
+const users = [
+    {
+        userName: 'midudev',
+        name: 'Miguel Ángel Durán',
+        isFollowing: true
+    },
+    {
+        userName: 'pheralb',
+        name: 'Pablo H.',
+        isFollowing: false
+    },
+    {
+        userName: 'PacoHdezs',
+        name: 'Paco Hdez',
+        isFollowing: true
+    },
+    {
+        userName: 'TMChein',
+        name: 'Tomas',
+        isFollowing: true
+    }
+]
 
 export function App() {
     return(
-        <article className='tw-followCard'>
-            <header className='tw-followCard-header'>
-                <img 
-                className='tw-followCard-avatar'
-                src="https://unavatar.io/midudev" />
-                <div className='tw-followCard-info'>
-                    <strong>Miguel Ángel Durán</strong>
-                    <span className='tw-followCard-infoUserName'>@midudev</span>
-                </div>
-            </header>
-
-            <aside>
-                <button className='tw-followCard-button'>
-                    Seguir
-                </button>
-            </aside>
-        </article>
+        <section className='App'>
+            {
+                users.map(({ userName, name, isFollowing }) => {
+                    return (
+                        <TwitterFollowCard 
+                            key = {userName}
+                            userName = {userName}
+                            isFollowing = {isFollowing}
+                        >
+                                {name}
+                        </TwitterFollowCard>
+                    )
+                })
+            }
+        </section>
     )
 }
