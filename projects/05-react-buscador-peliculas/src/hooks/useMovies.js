c6367ac836749c99672b5482b7fb6fb79968e65e
Luis <veachairezluisangel@gmail.com>
2025-02-17 17:13:08 -0700

    me quiero pegar un tiro, 3er proyecto hecho

---
+++ projects/05-react-buscador-peliculas/src/hooks/useMovies.js
@@ -0,0 +1,34 @@
+import { useCallback, useState, useMemo, useRef } from 'react'
+import { searchMovies } from '../services/movies'
+
+export function useMovies ({ search, sort }) {
+  const [movies, setMovies] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const prevSearch = useRef(search)
+
+  const getMovies = useCallback(async ({ search }) => {
+    if (prevSearch.current === search) return
+
+    try {
+      setLoading(true)
+      setError(null)
+      const newMovies = await searchMovies({ search })
+      setMovies(newMovies)
+      prevSearch.current = search
+    } catch (e) {
+      setError(e.message)
+    } finally {
+      setLoading(false)
+    }
+  }, [])
+
+  const sortedMovies = useMemo(() => {
+    if (!movies) return
+    return sort
+      ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
+      : movies
+  }, [sort, movies])
+
+  return { movies: sortedMovies, getMovies, loading }
+}
